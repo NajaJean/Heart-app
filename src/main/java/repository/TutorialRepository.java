4db098c9c89391b655f8b8a25c929aca5b209bbe
NajaJean <55448866+NajaJean@users.noreply.github.com>
2021-02-04 23:16:00 +0100

    Almost working vue and pom

---
+++ src/main/java/repository/TutorialRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long>   {
+	 List<Tutorial> findByPublished(boolean published);
+	 List<Tutorial> findByTitleContaining(String title);
+}
