b0fd847473dd32c41cec5c1197d25c3d175f2fc5
NajaJean <55448866+NajaJean@users.noreply.github.com>
2021-02-04 10:06:34 +0100

    First commit

---
+++ src/main/java/com/bachelor/Heartapp/HeartAppApplication.java
@@ -0,0 +1,13 @@
+package com.bachelor.Heartapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bachelor/Heartapp/HeartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bachelor.Heartapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
