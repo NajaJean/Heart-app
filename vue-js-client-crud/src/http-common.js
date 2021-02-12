60bba4cac6d4105127da21ecb7cc4e2539540f8b
NajaJean <55448866+NajaJean@users.noreply.github.com>
2021-02-12 17:38:37 +0100

    Massive delete of packages

--- vue-js-client-crud/src/http-common.js
+++ vue-js-client-crud/src/http-common.js
@@ -3,10 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://localhost:8080/api",
   headers: {
-    "Content-type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-    "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    "Content-type": "application/json"
   }
   
 });
--- vue-js-client-crud/vue.config.js
+++ vue-js-client-crud/vue.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   devServer: {
-    port: 8081,
-    headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-    "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-  }  
-}
+    port: 8081, 
+  }
 }
