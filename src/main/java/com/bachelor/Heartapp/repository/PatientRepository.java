db41f9f56a6d2d5ac3ba799d58cba390b84c5ad2
Mathilde T <55983203+s184376@users.noreply.github.com>
2021-03-24 12:12:44 +0100

    adding patient endpoints

---
+++ src/main/java/com/bachelor/Heartapp/repository/PatientRepository.java
@@ -0,0 +1,9 @@
+package com.bachelor.Heartapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bachelor.Heartapp.model.Patient;
+
+public interface PatientRepository extends JpaRepository<Patient, Long>   {
+
+}
