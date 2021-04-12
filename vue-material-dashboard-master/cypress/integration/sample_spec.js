ba9369cf200ddfc57f5394f9653ef17f357ba687
NajaJean <55448866+NajaJean@users.noreply.github.com>
2021-04-12 09:51:49 +0200

    Cypress

---
+++ vue-material-dashboard-master/cypress/integration/sample_spec.js
@@ -0,0 +1,10 @@
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
+
+it('First Test', () => {
+	cy.visit('http://localhost:8081/');
+	
+})
