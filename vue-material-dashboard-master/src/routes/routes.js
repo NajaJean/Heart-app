e6cbaa32555cb64a5be8e274c24631d1e237d0e4
NajaJean <55448866+NajaJean@users.noreply.github.com>
2021-04-14 13:51:16 +0200

    Log-in

--- vue-material-dashboard-master/src/routes/routes.js
+++ vue-material-dashboard-master/src/routes/routes.js
@@ -1,9 +1,9 @@
 import DashboardLayout from "@/pages/Layout/DashboardLayout.vue";
-
 import Dashboard from "@/pages/Dashboard.vue";
 import ECGLive from "@/pages/ECGLive.vue";
 import MeasurementsList from "@/pages/MeasurementsList.vue";
 import Notifications from "@/pages/Notifications.vue";
+import LogIn from "@/pages/LogIn.vue";
 
 const routes = [
   {
@@ -31,8 +31,44 @@ const routes = [
         name: "Notifications",
         component: Notifications
       }
+                    name: "Notifications",
     ]
   }
+/*
+const routes = [
+    {
+        path: "/",
+        component: LogIn,
+        children: [{
+            path: "/dashboardLayout",
+            name: "DashboardLayout",
+            component: DashboardLayout,
+            redirect: "/dashboard",
+            children: [
+                {
+                    path: "dashboard",
+                    name: "Dashboard",
+                    component: Dashboard
+                },
+                {
+                    path: "ecglive",
+                    name: "ECGLive",
+                    component: ECGLive
+                },
+                {
+                    path: "measurements",
+                    name: "Measurements",
+                    component: MeasurementsList
+                },
+                {
+                    path: "notifications",
+                    name: "Notifications",
+                    component: Notifications
+                }
+            ]
+        }
+        ]
+    }
 ];
-
+*/
 export default routes;
