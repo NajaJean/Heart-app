a919fc9cc5c142953ca5b3b71a717808245ffabe
mathildetannebaek <55983203+mathildetannebaek@users.noreply.github.com>
2021-04-29 17:02:18 +0200

    Update SideBar.vue

--- vue-material-dashboard-master/src/components/SidebarPlugin/SideBar.vue
+++ vue-material-dashboard-master/src/components/SidebarPlugin/SideBar.vue
@@ -47,7 +47,7 @@ export default {
   props: {
     title: {
       type: String,
-      default: "Heart Rater"
+      default: "HeartRater"
     },
     sidebarBackgroundImage: {
       type: String,
