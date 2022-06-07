bc19e1cf092ee7d7f659b9cc020864787b5283f9
NajaJean <55448866+NajaJean@users.noreply.github.com>
2022-06-07 15:06:11 +0200

    Ready for jenkins

--- vue-material-dashboard-master/src/http-common.js
+++ vue-material-dashboard-master/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://heartrater.live/api",
   withCredentials: false,
   headers: {
     "Access-Control-Allow-Origin": "*",
