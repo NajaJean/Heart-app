52467cf2c556dd1b4c471ee8048f77c6815019cd
Mathilde T <55983203+s184376@users.noreply.github.com>
2022-06-13 10:50:40 +0200

    remove unused

--- src/main/java/com/bachelor/Heartapp/controller/RealTimeController.java
+++ src/main/java/com/bachelor/Heartapp/controller/RealTimeController.java
@@ -3,8 +3,6 @@ package com.bachelor.Heartapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
