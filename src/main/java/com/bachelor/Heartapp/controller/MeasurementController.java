5bee4b637eaa5abbc9a0b58f9e56e662d0de1559
Mathilde T <55983203+s184376@users.noreply.github.com>
2022-06-22 11:13:25 +0200

    new n number of aau endpoint

--- src/main/java/com/bachelor/Heartapp/controller/MeasurementController.java
+++ src/main/java/com/bachelor/Heartapp/controller/MeasurementController.java
@@ -239,6 +239,51 @@ public class MeasurementController {
 		}
 	}
 
+	@GetMapping("/aau/measurements/{n}/{subject_id}/{pwd}/{measurement_type}")
+	public ResponseEntity<List<Measurement>> getPatientsNAauMeasurements(@PathVariable("n") int n,
+			@PathVariable("subject_id") String subject_id,
+			@PathVariable("pwd") String pwd,
+			@PathVariable("measurement_type") String measurement_type) {
+		try {
+			String mType = translateMeasurementType(measurement_type);
+
+			Unirest.setTimeouts(0, 0);
+			HttpResponse<String> response = Unirest.post("https://www.hjerteportalen.dk/api/v1/measured/data/")
+					.header("Accept-Encoding", "gzip, deflate, br")
+					.header("Connection", "keep-alive")
+					.header("Authorization", "HjerteData 6f69ec8ace5fa3c948792bd6cad3a501")
+					.field("SubjectId", subject_id)
+					.field("Pwd", pwd)
+					.field("Type", mType)
+					.field("Days", String.valueOf(n))
+					.asString();
+
+			String json = response.getBody();
+			Gson gson = new GsonBuilder().create();
+			AauMeasurement aauResult = gson.fromJson(json, AauMeasurement.class);
+
+			String patientid = Integer.toString(aauResult.getSubjectId());
+
+			List<Measurement> res = new ArrayList<Measurement>();
+
+			for (int i = 0; i < aauResult.getNvals(); i++) {
+				AauValue v = aauResult.getValues()[i];
+				Date datepost = new SimpleDateFormat("yyyy-MM-dd").parse(v.getDateRec());
+				Measurement m = new Measurement(patientid, datepost, measurement_type, v.getValue());
+
+				res.add(m);
+			}
+			System.out.println(res);
+
+			if (res.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<>(res, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 	private String translateMeasurementType(String measurementType) {
 		int mtype;
 		switch (measurementType) {
